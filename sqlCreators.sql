5f681b0096521f01c8cae60900eb526f2d207f72
RolandoAndrade <rolandoandradefernandez@gmail.com>
2018-12-27 12:17:58 -0400

    Usando Docker: Prueba 1

--- sqlCreators.sql
+++ sqlCreators.sql
@@ -7,27 +7,9 @@ CREATE TABLE users (
   email VARCHAR
 );
 
-INSERT INTO users (first_name, first_surname, second_surname,email)
-  VALUES ('Rolando', 'Andrade', 'Fernández','rolandoandradefernandez@gmail.com');
-
-SELECT * FROM  users
-
-
-select * from users
-where LOWER(second_name)  like LOWER('')
-order by ID
-
-
 CREATE TABLE transactions (
   ID SERIAL PRIMARY KEY,
   customer INTEGER,
   date DATE,
   amount FLOAT
 );
-
-SELECT * FROM  transactions
-
-INSERT INTO transactions (customer, date, amount)
-  VALUES (1, '2018-12-16', 10.3);
-
-select * from transactions where customer=7
